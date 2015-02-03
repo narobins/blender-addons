3046d7e0187d60f298cf53dfd48a382e6bf03189
nellie-robinson <nellie_robinson@brown.edu>
2015-02-02 22:10:22 -0500

    added randomize function
    also cleaned up code a little

--- tree_generate.py
+++ tree_generate.py
@@ -10,11 +10,12 @@ bl_info = {
 import bpy
 import bmesh
 from bpy.props import IntProperty, FloatProperty, BoolProperty, FloatVectorProperty, CollectionProperty
-from random import uniform, random 
+from random import uniform, random, getrandbits
 from mathutils import Vector, Matrix   
 from math import pi, cos, sin, acos, asin, sqrt, fabs, degrees, radians
 from copy import copy
 
+# creates spline (branch segment)
 def makeSpline(cu, typ, points, weights):
 	spline = cu.splines.new(typ)
 	npoints = len(points)
@@ -31,19 +32,19 @@ def makeSpline(cu, typ, points, weights):
 	spline.use_endpoint_u = True
 	return
 
-def makeSphere(coords, w):
-	scn.objects.active = sph_ob
-	bpy.ops.object.mode_set(mode='EDIT')
-	bpy.ops.mesh.primitive_uv_sphere_add(location = coords, size = w/20)
-	bpy.ops.object.mode_set(mode='OBJECT')
-	scn.objects.active = ob
-
+# calculates inner product of vector
 def inner_product(v):
 	return sqrt(sum([x**2 for x in v]))
 
+# calculates norm of vector
 def norm(v):
 	return v / inner_product(v)
 
+# returns random vector
+def rand_vec(minimum, maximum, length):
+	return Vector([uniform(minimum, maximum) for l in range(length)])
+
+# returns matrix of rotation
 def rotation_mat(axis, angle):
 	c = cos(angle)
 	s = sin(angle)
@@ -55,12 +56,13 @@ def rotation_mat(axis, angle):
 				[t*x*y + s*z, t*(y**2)+c, t*y*z - s*x],
 				[t*x*z - s*y, t*y*z + s*x, t*(z**2) + c]])
 
+# multiplies vector by scalar
 def vector_mult(a, b):
 	return Vector([b * x for x in a])
 
+# multiplies two vectors
 def vec_vec_mult(a, b):
 	return Vector([x * y for (x, y) in zip(a, b)])
-
 
 
 class Branch(object):
@@ -71,6 +73,7 @@ class Branch(object):
 		self.l = l
 		self.w = w
 	
+	# testing purposes
 	def __str__(self):
 		return "{}, {}, {}, {}".format([x for x in self.co],self.w,self.l,[degrees(x) for x in self.a])
 
@@ -83,6 +86,7 @@ class TreeGenerator(bpy.types.Operator):
 
 	l0 = FloatProperty(
 		name = "length",
+		description = "length of initial branch",
 		default = 10,
 		min = 1,
 		max = 50,
@@ -90,20 +94,23 @@ class TreeGenerator(bpy.types.Operator):
 	
 	w0 = FloatProperty(
 		name = "weight",
+		description = "weight of initial branch",
 		default = 20,
 		min = 1,
 		max = 100,
 	)
 
 	lr = FloatProperty(
-		name = "elongation rate",
+		name = "shortening rate",
+		description = "rate at which length decreases",
 		default = 1.109,
 		min = 0.5,
 		max = 1.5,
 	)
 
 	vr = FloatProperty(
-		name = "width decrease rate",
+		name = "weight decrease rate",
+		description = "rate at which weight decreases",
 		default = 1.732,
 		min = 1.2,
 		max = 2,
@@ -111,6 +118,7 @@ class TreeGenerator(bpy.types.Operator):
 
 	nr = IntProperty(
 		name = "segments per branch",
+		description = "used for curving branches",
 		default = 3,
 		min = 2,
 		max = 5,
@@ -118,6 +126,7 @@ class TreeGenerator(bpy.types.Operator):
 
 	ba1 = FloatProperty(
 		name = "max",
+		description = "max branching angle",
 		default = pi/3,
 		min = pi/4,
 		max = pi/2,
@@ -125,6 +134,7 @@ class TreeGenerator(bpy.types.Operator):
 
 	ba2 = FloatProperty(
 		name = "min",
+		description = "min branching angle",
 		default = pi/12,
 		min = pi/16,
 		max = pi/4,
@@ -132,6 +142,7 @@ class TreeGenerator(bpy.types.Operator):
 
 	prune = BoolProperty(
 		name = "prune",
+		description = "cuts off branches beyond certain radius",
 		default = False,
 	)
 
@@ -155,21 +166,24 @@ class TreeGenerator(bpy.types.Operator):
 
 	wr = FloatProperty(
 		name = "lumpiness",
-		default = 1,
-		min = 1,
-		max = 2,
+		description = "random variations in branch weight",
+		default = 0,
+		min = 0,
+		max = 1,
 	)
 
 	cr = FloatProperty(
 		name = "curvature",
-		default = 1,
-		min = 1,
-		max = 2,
+		description = "random variations in vertices of branch segments",
+		default = 0,
+		min = 0,
+		max = 1,
 	)
 
 
 	twig_threshold = FloatProperty(
 		name = "twig threshold",
+		description = "required weight of branch to stop recursion",
 		default = 0.6,
 		min = 0.2,
 		max = 3.0,
@@ -177,25 +191,52 @@ class TreeGenerator(bpy.types.Operator):
 
 	gradual_angle = FloatVectorProperty(
 		name = "gradual angle",
+		description = "angles branches towards certain angle",
 		size = 3,
 		default = (1, 1, 1),
-		min = 0.01,
-		max = 1.99,
+		min = 0.5,
+		max = 1.5,
 	)
 		
 	gradual_scale = FloatVectorProperty(
 		name = "gradual scale",
+		description = "scales branches in certain dimensions",
 		size = 3,
 		default = (1, 1, 1),
 		subtype = 'XYZ',
-		min = 0.01,
-		max = 1.99,
+		min = 0.5,
+		max = 1.5,
 	)
 
-	cr2 = 1 
-	wr2 = 1
+	straight_branches = BoolProperty(
+		name = "continuous branches",
+		description = "whether the main branch in intersection curves",
+		default = False,
+	)
+
+	randomize = BoolProperty(
+		name = "randomize",
+		description = "randomizes parameters (may produce slow and/or unexpected results",
+		default = False,
+	)
 
 
+	# randomizes parameters to tree generator
+	def randomize_all(self):
+		self.l0 = uniform(5, 50)
+		self.w0 = uniform(5, 80)
+		self.lr = uniform(.6, 1.4)
+		self.vr = uniform(1.3, 2)
+		self.nr = uniform(2, 5)
+		self.ba1 = uniform(pi/4, pi/2)
+		self.ba2 = uniform(pi/16, pi/4)
+		self.wr = uniform(0, 1)
+		self.cr = uniform(0, 1)
+		self.twig_threshold = uniform(0.5, 3.0)
+		self.gradual_scale = rand_vec(.6, 1.4, 3)
+		self.gradual_angle = rand_vec(.6, 1.4, 3)
+
+	# returns points and weights of a curved branch
 	def curve_branch(self, co1, co2, w1, w2, n):
 		diff = co2 - co1
 		diff_w = w2 - w1
@@ -207,6 +248,7 @@ class TreeGenerator(bpy.types.Operator):
 				[(w1 + (i/(n - 1)) * diff_w) * (uniform(self.wr, self.wr2) if i != 0 and i != n-1 else 1)
 				 for i in range(n)])
 
+	# draws spline and returns its endpoint
 	def forward(self, br):
 		target = copy(br)
 		target.co = br.co + vec_vec_mult(vector_mult(br.a, br.l), self.gradual_scale)
@@ -216,6 +258,7 @@ class TreeGenerator(bpy.types.Operator):
 		target.co = Vector([coords[self.nr-1].x, coords[self.nr-1].y, coords[self.nr-1].z])
 		return target
 
+	# returns next branch 
 	def calcBranch(self, branch, angle=0):
 		rotation = uniform(0, pi * 2)
 		br = copy(branch) 
@@ -236,7 +279,7 @@ class TreeGenerator(bpy.types.Operator):
 		br.a = norm(vec_vec_mult(br.a, self.gradual_angle))
 		return br
 
-	# whether this should be pruned
+	# whether this branch should be pruned
 	def to_prune(self, coord):
 		for (c, n) in pruning_pts:
 			if (coord - c).length < n:
@@ -247,7 +290,7 @@ class TreeGenerator(bpy.types.Operator):
 	def min_prune(self, coord):
 		return max([(n - (coord - c).length, n) for (c, n) in pruning_pts if (coord - c).length < n])
 			
-
+	# main function; draws branch and keeps recurring
 	def branch(self, br):
 		if (br.w > self.twig_threshold and (not self.prune or not self.to_prune(br.co))):
 			if self.prune:
@@ -263,8 +306,12 @@ class TreeGenerator(bpy.types.Operator):
 				self.branch(self.calcBranch(branch=target, angle = uniform(self.ba1, self.ba2)))
 			if (choice > 0.2):   
 				self.branch(self.calcBranch(branch=target, angle = uniform(self.ba1, self.ba2)))
-			self.branch(self.calcBranch(branch=target, angle=0))
+			if (self.straight_branches):
+				self.branch(self.calcBranch(branch=target, angle=0))
+			else:
+				self.branch(self.calcBranch(branch=target, angle=uniform(0, self.ba1/2)))
 
+	# menu layout
 	def draw(self, context):
 		layout = self.layout
 		# initial characteristics
@@ -284,6 +331,7 @@ class TreeGenerator(bpy.types.Operator):
 		row1.label("angle")
 		row1.prop(self, 'ba1')
 		row1.prop(self, 'ba2')
+		box4.prop(self, 'straight_branches')
 		# pruning
 		box = layout.box()
 		box.label("Pruning")
@@ -301,8 +349,9 @@ class TreeGenerator(bpy.types.Operator):
 		box2.label("Skew")
 		box2.prop(self, 'gradual_angle')
 		box2.prop(self, 'gradual_scale')
+		layout.prop(self, 'randomize')
 
-
+	# when the operator is applied
 	def execute(self, context):
 		global cu
 		global ob
@@ -317,6 +366,16 @@ class TreeGenerator(bpy.types.Operator):
 		cu.fill_mode = 'FULL'
 		cu.bevel_depth = 1.0
 		cu.bevel_resolution = 3
+
+		if (self.randomize):
+			self.randomize_all()
+			self.randomize = False
+
+		self.cr += 1
+		self.wr += 1
+		self.cr2 = 1 - self.cr 
+		self.wr2 = 1 - self.wr 
+
 		if self.prune:
 			global pruning_pts
 			pruning_pts = [(init_loc + Vector([a, b, c]), d) 
